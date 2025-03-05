803a6af01939631a1b7d3cd05923d34b96c65720
Alexandre Marques <alexandreacm.developer@gmail.com>
2025-03-05 20:20:06 -0300

    Created my map view and search textField

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -10,12 +10,73 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    private lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        map.showsUserLocation = true
+        
+    
+        return map
+    }()
+    
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.clipsToBounds = true
+        textField.placeholder = "Search"
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 10
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.returnKeyType = .go
+        
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.green
+        setupView()
     }
 
+    
+    private func setupView(){
+//        view.backgroundColor = UIColor.green
+        setHierarchy()
+        setConstrants()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(searchTextField)
+        view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
+    }
+    
+    private func setConstrants(){
+        let widthAnchor = mapView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        let heightAnchor = mapView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        
+        widthAnchor.isActive = true
+        heightAnchor.isActive = true
+        
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            searchTextField.heightAnchor.constraint(equalToConstant: 44),
+            searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width / 1.2)
+        ])
+        
+//        NSLayoutConstraint.activate([
+//            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+//            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//        ])
+    }
 
 }
 
