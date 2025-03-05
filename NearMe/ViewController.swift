507fb73d05523246c9c1ccf8d2f3d11a8318c3ae
Alexandre Marques <alexandreacm.developer@gmail.com>
2025-03-05 19:11:50 -0300

    first commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Alexandre Marques on 3/5/25.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.green
+    }
+
+
+}
+
